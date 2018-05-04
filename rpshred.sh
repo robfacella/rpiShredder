55c9b401622fcc00983281c1f755d7f9bc512d5f
Robert Facella <rob.facella@gmail.com>
2018-05-04 11:38:22 -0400

    Only shred unique drives. (Don't re-shred for each partition found.)

--- rpshred.sh
+++ rpshred.sh
@@ -3,9 +3,9 @@ echo "Be very careful."
 echo "This program is designed to run shred over all USB devices attached to a Raspberry Pi. (All /dev/sd* devices.)"
 echo "Do not run this if you do not understand what it does or how it works."
 echo "I will not be held responsible for data-loss due to misuse."
-echo "root permissions required"
+echo "root permissions required for using shred on entire devices and for reformatting."
 
-for drive in ` df | grep /dev/sd | awk '{ variable = sprintf("%-.8s", $1); print variable}'`
+for drive in ` df | grep /dev/sd | awk '{ variable = sprintf("%-.8s", $1); print variable}' | uniq`
 do
   echo "Overwrite $drive with 0s:"
   shred -fvz -n 0 $drive
